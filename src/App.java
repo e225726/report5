a86eb9a3404615bc6627d03714ba298f42ba7c17
Asato Kazuchika <e225726@ie.u-ryukyu.ac.jp>
2023-02-06 17:52:33 +0900

    2nd commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }catch (NullPointerException n){
+            System.out.println("NullPointerExceptionが発生した");
+            System.out.println(n.getMessage());
+        }
     }
 }
